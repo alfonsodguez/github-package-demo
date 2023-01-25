a598e197839643e6ea02ff7285e1fcd94d76a00d
alfonsodguez <alfonsoalejandro33@gmail.com>
2023-01-25 21:27:05 +0100

    proof my package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import mypackage from '@alfonsodguez/nodejs-github-package/src/index.js'
 
 function checkSum(num1, num2, result) {
-    const sumValue = num1 + num2
+    const sumValue = mypackage.sum(num1, num2)
     
     if (sumValue === result) {
         return 'OK, is correct'
